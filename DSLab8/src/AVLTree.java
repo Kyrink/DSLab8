91c28097b9e3b7ba1d97ce77c8d1018f6bab4733
Kyrin Kalonji <kyrink05@gmail.com>
2023-03-29 10:47:29 -0400

    Added the getHeight, getBalance, left and right rotation method

--- DSLab8/src/AVLTree.java
+++ DSLab8/src/AVLTree.java
@@ -11,5 +11,43 @@ public class AVLTree {
     }
 
     private Node root;
+    
+    // Helper methods for balancing the tree
+    private int getHeight(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.height;
+    }
+
+    
+    private int getBalance(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return getHeight(node.left) - getHeight(node.right);
+    }
+
+    private Node leftRotate(Node x) {
+        Node y = x.right;
+        Node T2 = y.left;
+        y.left = x;
+        x.right = T2;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        return y;
+    }
+    
+    private Node rightRotate(Node y) {
+        Node x = y.right;
+        Node T2 = x.left;
+        x.left = y;
+        y.right = T2;
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+        return y;
+    }
+    
+
 
 }
